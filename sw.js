ff0ffdb432a1bded024dabe5db11b74185026c93
radinotech-beep <radinotech@gmail.com>
2026-06-25 19:42:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pepipepu-v8-email-image-quality';
+const CACHE = 'pepipepu-v9-pdf-link-email';
 const FILES = ['./index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
