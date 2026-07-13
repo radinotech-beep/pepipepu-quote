94671b854a80fc5663a89a680ad9148db92c418d
radinotech-beep <pepipepu2020@gmail.com>
2026-07-13 10:27:46 +0900

    Prioritize rack post orientation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pepipepu-v34-qp-hole-detail';
+const CACHE = 'pepipepu-v35-post-direction-priority';
 const FILES = ['./index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
