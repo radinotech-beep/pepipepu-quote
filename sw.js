486aec3fdd47f2b523ee28dfc56deb422fc11060
radinotech-beep <pepipepu2020@gmail.com>
2026-07-14 00:40:56 +0900

    Add quote grouping

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pepipepu-v46-brace-code';
+const CACHE = 'pepipepu-v47-groups';
 const FILES = ['./index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
