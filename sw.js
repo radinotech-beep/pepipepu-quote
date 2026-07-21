dbcd463ca6a341713d67e67f0f3e1d7ec2ad0b30
radinotech-beep <pepipepu2020@gmail.com>
2026-07-21 10:26:58 +0900

    Add coated board option

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pepipepu-v47-groups';
+const CACHE = 'pepipepu-v48-board-option';
 const FILES = ['./index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
